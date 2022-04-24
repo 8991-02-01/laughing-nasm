484cb0c76164a6addb28ae65e088d9e1f025198b
Steven Fernandez <sfernandez0201@gmail.com>
2022-04-24 15:48:59 -0400

    sub routines but broken

--- helloworld-len.asm
+++ helloworld-len.asm
@@ -8,17 +8,9 @@
 
 _start:
     mov ecx, msg                ; msg into ecx register for writing
-    mov esi, ecx                ; esi points to same pool in memory as ecx
+    call _do_count_chars      ; count chars
 
-_loop_count_chars:
-    cmp byte [esi], 0           ; compare the byte pointed to by esi against zero (Zero is end of string delimiter)
-    jz _end                     ; jump (if zero flagged has been set to _end)
-    inc esi                     ; inc esi by one byte
-    jmp _loop_count_chars       ; eat up next bytes
-
-_end:
-    sub esi, ecx                ; esi has number of bytes until end of ecx which contains the msg
-    mov edx, esi                ; edx will contain count required to exec write syscall
+    ;; _end
     mov ebx, 1                  ; 1 is stdout?
     mov eax, 4                  ; write
     int 80h
@@ -26,3 +18,19 @@ _end:
     mov ebx, 0                  ; 0 is success
     mov eax, 1
     int 80h
+
+_do_count_chars:
+    push esi                    ; esi memory addresss
+    mov esi, ecx                ; esi loads address of ecx
+
+_count_chars:
+    cmp byte [esi], 0           ; compare the byte pointed to by esi against zero (Zero is end of string delimiter)
+    jz _end_count_chars         ; jump zero flag or some shit like that
+    inc esi                     ; inc esi by one byte
+    jmp _count_chars       ; eat up next bytes
+
+_end_count_chars:               ; leverage the stack that's the point
+    pop edi                     ; pop the result of count chars into edi
+    sub edi, ecx                ; ecx = string
+    mov edx, edi                ; mov into edx the result of that
+    ret
